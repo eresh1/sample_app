794f6b0c3f09a1a6ad2020c421c96cbef34095e7
eresh1 <eresh1@umbc.edu>
2012-04-04 15:46:53 -0400

    Through Section 8.4.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
     @user = User.new
     @title = "Sign up"
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+	flash[:success] = "Welcome to the Sample App!"
+	redirect_to @user
+    else
+	@title = "Sign Up"
+	render 'new'
+    end
+  end
 end

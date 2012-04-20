0bff1d555e4c7f1661cca44146b14034f0402fa8
eresh1 <eresh1@umbc.edu>
2012-04-19 21:13:16 -0400

    Cleared out USer Spec file for testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
-	before_filter :authenticate, :only => [:edit, :update]
+	before_filter :authenticate, :only => [:index, :edit, :update]
+	before_filter :correct_user, :only => [:edit, :update]
+
+ def index
+    @title = "All users"
+    @users = User.all
+  end
 
   def show
-	@user = User.find(params[:id])
-	@title = @user.name
+    @user = User.find(params[:id])
+    @title = @user.name
   end
 
   def new
@@ -31,7 +37,7 @@ class UsersController < ApplicationController
  end
   
   def edit 
-	@user = User.find(params[:id])
+	#@user = User.find(params[:id])
 	@title = "Edit user"
   end
 
@@ -51,6 +57,11 @@ private
 
     def authenticate
       deny_access unless signed_in?
+    end
+    
+     def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
 

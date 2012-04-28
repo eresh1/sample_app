5a175146ea9fc3abdc17b1e45724f9eb4b599834
eresh1 <eresh1@umbc.edu>
2012-04-28 13:33:33 -0400

    Through Section 11.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,15 +18,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
 
 	def new
-		
-		@user = User.new
-		@title = "Sign up"
-	
-  end
+	@user = User.new
+	@title = "Sign up"
+	end
   
 
   def create

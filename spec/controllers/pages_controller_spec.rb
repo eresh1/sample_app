97d44970dc5e7022a9076e16546a5293236f534f
eresh1 <eresh1@umbc.edu>
2012-05-03 20:02:42 -0400

    Finished thru 12.3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,41 +3,65 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
-  before(:each) do
-    #
-    @base_title = "Ruby on Rails Tutorial Sample App"
-    #
-  end
+  	before(:each) do
+    	#
+    	@base_title = "Ruby on Rails Tutorial Sample App"
+    	#
+  	end
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
+  	describe "GET 'home'" do
+    		describe "when not signed in" do
 
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-                                    :content => @base_title + " | Home")
-    end
+      			before(:each) do
+        		get :home
+      			end
+
+      			it "should be successful" do
+        		response.should be_success
+      			end
+
+      			it "should have the right title" do
+        		response.should have_selector("title",
+                                      :content => "#{@base_title} | Home")
+      			end
+    		end
+
+		describe "when signed in" do
+
+      			before(:each) do
+        		@user = test_sign_in(Factory(:user))
+        		other_user = Factory(:user, :email => Factory.next(:email))
+        		other_user.follow!(@user)
+      			end
+
+      			it "should have the right follower/following counts" do
+        		get :home
+        		response.should have_selector("a", :href => following_user_path(@user),
+                                           :content => "0 following")
+        		response.should have_selector("a", :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      			end
+    		end
+  	end
+
 
 		#Exercise 2 Chapter 11
-			describe "for signed-in users" do
+			#describe "for signed-in users" do
 
-			before(:each) do
-			@user = test_sign_in(Factory(:user))
-			@mp1 = Factory(:micropost, :user => @user)
-			end
-
-			it "should have micropost count in sidebar" do
-			get :home
-			response.should have_selector("span.microposts", :content => "1 micropost")
-			@mp2 = Factory(:micropost, :user => @user)
-			get :home
-			response.should have_selector("span.microposts", :content => "2 microposts")
-			end
-			end
-			end
+			#before(:each) do
+#			@user = test_sign_in(Factory(:user))
+#			@mp1 = Factory(:micropost, :user => @user)
+#			end
+#
+#			it "should have micropost count in sidebar" do
+#			get :home
+#			response.should have_selector("span.microposts", :content => "1 micropost")
+#			@mp2 = Factory(:micropost, :user => @user)
+#			get :home
+#			response.should have_selector("span.microposts", :content => "2 microposts")
+#			end
+#			end
+#			end
 
   describe "GET 'contact'" do
     it "should be successful" do
